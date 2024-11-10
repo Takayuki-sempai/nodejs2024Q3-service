0045bc10a9666a00c457607b76e674f01d8676a6
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-11-10 04:02:28 +0300

    feat: add port to .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(4000);
+  const port = +process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
