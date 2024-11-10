452a9a2640571b21a77f299345506dcf79a2ff83
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-11-10 05:05:32 +0300

    feat: add open api

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import * as Yaml from 'yaml';
 import 'dotenv/config';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = Yaml.parse(
+    (await readFile(join(process.cwd(), 'doc/api.yaml'))).toString('utf-8'),
+  );
+  SwaggerModule.setup('doc', app, document);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = +process.env.PORT || 4000;
   await app.listen(port);
